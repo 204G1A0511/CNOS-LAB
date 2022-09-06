d1d6a02c4ad2212053c674b12cfdb222d3f37150
204G1A0511 <94515088+204G1A0511@users.noreply.github.com>
2022-09-06 12:36:47 +0530

    Create sample.c
    B Anand Created Sample.c

---
+++ OSLAB/sample.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B Anand
+   Roll No: 204G1A0511
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
